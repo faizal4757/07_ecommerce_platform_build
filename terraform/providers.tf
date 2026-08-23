72a704dc55e2cebdc48150b49166af4938249c23
faizal4757 <faizalahmedmotog3@gmail.com>
2026-08-23 13:38:30 +0530

    added aws provider and versions to terraform to manage aws resources like s3 through terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,9 @@
 # Authentication is supplied by Databricks unified authentication, using the
 # local, ignored DATABRICKS_HOST and DATABRICKS_TOKEN environment variables.
 # Do not add credentials to this file.
+
 provider "databricks" {}
+
+provider "aws" {
+  region = "us-east-1"
+}
