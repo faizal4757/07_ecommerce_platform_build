050ce119546bf4d92cb8b258a5e72049dd27bd87
faizal4757 <faizalahmedmotog3@gmail.com>
2026-08-23 16:00:25 +0530

    created aws s3 access to get access to s3 through databricks, use s3allacces for simplicity

---
+++ terraform/iam.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_role" "databricks_olist_access" {
+  name = "databricks-olist-access"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          AWS = "arn:aws:iam::414351767826:role/unity-catalog-prod-UCMasterRole-14S5ZJVKOTYTL"
+        }
+
+        Action = "sts:AssumeRole"
+
+        Condition = {
+          StringEquals = {
+            "sts:ExternalId" = "a9563243-0e5c-404b-9dbb-db588d9a03e2"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Project     = "Olist Data Platform"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+    Purpose     = "Databricks Unity Catalog S3 Access"
+  }
+}
+
+resource "aws_iam_role_policy" "databricks_olist_s3" {
+  name = "databricks-olist-s3-access"
+  role = aws_iam_role.databricks_olist_access.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = "s3:*"
+
+        Resource = [
+          aws_s3_bucket.olist_data.arn,
+          "${aws_s3_bucket.olist_data.arn}/*"
+        ]
+      }
+    ]
+  })
+}
