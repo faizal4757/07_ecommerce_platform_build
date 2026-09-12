6d7d26b6ede8b68f11ab0313554bf98a7a5fb70d
faizal4757 <faizalahmedmotog3@gmail.com>
2026-09-12 19:34:18 +0530

    feat: complete bronze ingestion for all datasets

--- src/jobs/bronze_ingestion.py
+++ src/jobs/bronze_ingestion.py
@@ -9,6 +9,7 @@ parser.add_argument("--source-path", required=True)
 parser.add_argument("--checkpoint-path", required=True)
 parser.add_argument("--schema-path", required=True)
 parser.add_argument("--target-table", required=True)
+parser.add_argument("--file-pattern", required=True)
 
 args = parser.parse_args()
 
@@ -16,6 +17,7 @@ args = parser.parse_args()
 query = ingest_to_bronze(
     spark=spark,
     source_path=args.source_path,
+    file_pattern=args.file_pattern,
     checkpoint_path=args.checkpoint_path,
     schema_path=args.schema_path,
     target_table=args.target_table
